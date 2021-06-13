2765775cf595cc5a05a5b26f6101a13bb180b226
Dashkata <alexandur04@abv.bg>
2021-06-13 22:52:12 +0300

    Configurations file

---
+++ BattleShips.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+
+
+
+int main(){
+
+    FILE *fp; 
+    int c;
+    fp = fopen("BattleShipsConfigurations.txt", "r");
+    while(1){
+        c = fgetc(fp);
+        if( feof(fp) ){
+            break;
+        }
+        printf("%c", c);
+        
+
+
+    }
+    fclose(fp);
+    return 0;
+}
