da88961d721c229b56cab1a75129b25f6c4257af
Dashkata <alexandur04@abv.bg>
2021-06-14 17:45:05 +0300

    adding new board file

--- BattleShipsConfigurations.h
+++ BattleShipsConfigurations.h
@@ -1,36 +1,29 @@
+// #include <stdio.h>
+
 #include <stdio.h>
 void board(){
-    int boardArray[11][11];
-    
-    for(int i = 0; i < 11; i++){
+    char boardArray[11][11];
+    for(int i = 0; i < 11 ;i ++){
         if(i != 10){
             printf(" ");
         }
-        for(int j = 0; j < 10; j++){
-            
+        for(int j = 0; j < 11; j++){
             if(i == 0){
-                
-                
                 if(j == 0){
                     printf(" |");
+
                 }
-                printf("%c|", 'A' + j );            
-            
-            
-            }else if(j == 0){
+                else{
+                    printf("%c|", 'A' + j - 1);
+                }
+
+            }else if(j == 0) {
                 printf("%d|", i);
             }else{
                 printf(" |");
             }
-        }
-        if(i != 0){
-            printf(" |");
+
         }
         printf("\n");
-        
-        
-        
-       
     }
-    
 }
