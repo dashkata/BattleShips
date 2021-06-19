9cc7d8aec6de117f85eec657103de8442e957537
bozhiinov <bozhiinov@gmail.com>
2021-06-19 22:49:15 +0300

    updates on player options

--- logic.c
+++ logic.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "configurations.h"
 
 struct player
@@ -7,6 +8,8 @@ struct player
     char board[11][11];
     char name[15];
 };
+
+int counter = 1;
 
 int start_game(struct player* player1, struct player* player2)
 {
@@ -73,11 +76,24 @@ void previous_strikes(/*masiv koito pazi opitite*/)
     // printi starite opiti
 }
 
-int strike(/*char* board, char* turns*/)
+int strike(struct player* player1, struct player* player2 /*char* turns*/)
 {
     int turn_decision = 0;
     int strike_decision = 0;
     int strike_success = 0;
+    char shot[3];
+    char direction[2];
+
+    struct player* turn = malloc(sizeof(struct player));
+    if(counter % 2 == 0){
+        strcpy(turn->name, player2->name);
+        strcpy(turn->board, player2->board);
+        //puts(turn->name);
+   }else{
+        strcpy(turn->name, player1->name);
+        strcpy(turn->board, player1->board);
+   }
+
 
     printf("Choose an option:\n1.See your previous turns.\n2.Shoot\n");
     scanf("%d", &turn_decision);
@@ -86,23 +102,29 @@ int strike(/*char* board, char* turns*/)
         case 1: previous_strikes();
                 break;
         case 2:
-            printf("\nHow do you want to shoot?\n1. Choose new coordinates\n2. Use your previous and choosing only direction to go for.\n");
+            printf("\nHow do you want to shoot?\n1. Choose new coordinates\n2. Use your previous shot and choose only a direction to go for.\n");
             scanf("%d", &strike_decision);
 
             switch(strike_decision)
             {
                 case 1: //izbira novi koordinati
+                    printf("OK, %s! Now shoot!(e.g. B4)\n", turn->name);
+                    scanf("%s", shot);
+                    printf("%s", shot);
                     break;
                 case 2: // polzva starata kletka i izbira posoka
+                    printf("OK, your previous shot was at neshto.\nChoose a direction to shoot! (Type L for left, R for right, U for up and D for down)\n");
+                    scanf("%s", direction);
+                    printf("%s", direction);
                     break;
                 default:
                     printf("\nUnfortunately you chose something invalid so you need to try again!\n");
-                    strike();
+                    strike(player1, player2);
                     break;
             }
             break;
         default: printf("Invalid decision! Try again!\n");
-                 strike();
+                 strike(player1, player2);
                  break;
     }
 
@@ -110,16 +132,17 @@ int strike(/*char* board, char* turns*/)
     return strike_success;
 }
 
-void strike_success(int success)
+void strike_success(int success, struct player* player1, struct player* player2)
 {
     if(success = 1){
         printf("\nGood job mate! You hit something and it's your turn again!\n");
         // ako cql korab e potopen
         // printf("That's not everything tho! Not only did you hit something but you submerged one of your opponent's ships!")
-        strike();
+        strike(player1, player2);
     }
     else{
         printf("\nSorry mate, you didn't hit any of your opponent's ships :(\n");
+        counter++;
         // dava na sledvashtiq igrach da igrae
     }
 }
@@ -147,11 +170,11 @@ int main()
     struct player player1;
     struct player player2;
     int game = start_game(&player1, &player2);
-    printf("%s, %s", player1.name, player2.name);
+    //printf("%s, %s", player1.name, player2.name);
 
     // spored tipa igra da prenasochva kum hodovete suotvetno na igrachite ili igracha i random hod za bota
-    //int str_succ = strike(/*&turns*/);
-    //strike_success(str_succ);
+    int str_succ = strike(&player1, &player2/*&turns*/);
+    strike_success(str_succ, &player1, &player2);
 
     return 0;
 }
